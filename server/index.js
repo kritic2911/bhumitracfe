69bd6d005864f183d1e39ac7450e083c77431586
kritic2905 <kritic2905>
2026-04-17 11:02:20 +0530

    render fix

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // Ensure preflight requests are handled for all routes.
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 
 app.post("/admin/login", (req, res) => {
